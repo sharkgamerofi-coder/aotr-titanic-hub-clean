7385d3e83ec4f1023753aa89ac144b92b0aa64d7
sharkgamerofi-coder <sharkgamerofi@gmail.com>
2026-08-26 14:48:27 -0600

    Add loader that concatenates clean script parts

---
+++ loader.lua
@@ -0,0 +1,5 @@
+-- AOTR Titanic Hub Clean (logger removed)
+local base = "https://raw.githubusercontent.com/sharkgamerofi-coder/aotr-titanic-hub-clean/main/"
+local a = game:HttpGet(base .. "part1.lua")
+local b = game:HttpGet(base .. "part2.lua")
+loadstring(a .. b)()
